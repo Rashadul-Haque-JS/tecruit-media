43c10ffd5f7346e362c3bbf4d436aae1093cdbbc
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-10-13 10:49:01 +0200

    Adjusted navbar padding

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
 
   return (
     <div
-      className="relative flex justify-between items-center gap-2 py-1 sm:py-2 md:py-2 px-6 sm:pl-0 md:pr-4 md:pl-0 sm:pr-4 text-tecruitPrimary bg-[#fff] z-50"
+      className="relative flex justify-between items-center gap-2 py-2 sm:py-3 md:py-3  px-6 sm:pl-0 md:pr-4 md:pl-0 sm:pr-4 text-tecruitPrimary bg-[#fff] z-50"
       style={{
         borderBottom: "none",
         boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
@@ -37,11 +37,11 @@ const Navbar = () => {
           <NavMenu title="Resources" links={linksTwo} />
         </div>
       </div>
-      <div className="flex justify-center items-center gap-4">
-        <Link to="/auth" className="border-r-2 px-4 py-2 sm:hidden border-gray-300 mt-2">
+      <div className="flex justify-center items-center gap-4 ">
+        <Link to="/auth" className="border-r-2 px-4 py-2 sm:hidden border-gray-300 ">
           Login
         </Link>
-        <Link className=" px-2 py-1 sm:hidden rounded border-gray-700 mt-2">
+        <Link className=" px-2 py-1 sm:hidden rounded border-gray-700 ">
           Join Us
         </Link>
         <div className=" hidden sm:flex md:block items-center text-xl">
@@ -59,9 +59,9 @@ const Navbar = () => {
             onClick={toggleDrawer}
           >
             {openDropDownDrawer ? (
-              <FontAwesomeIcon icon={faXmark} className="border border-gray-300 px-2 py-1 md:border-none" />
+              <FontAwesomeIcon icon={faXmark} className="border border-gray-300 px-2 py-1 " />
             ) : (
-              <FontAwesomeIcon icon={faBars} className="border border-gray-300 px-2 py-1 md:border-none" />
+              <FontAwesomeIcon icon={faBars} className="border border-gray-300 px-2 py-1 " />
             )}
           </button>
         </div>
