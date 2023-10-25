8cf9eddc5ffe427621b4a760c89d1fde4e1a5ebc
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-10-25 21:25:27 +0200

    Modified sevice-worker'

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,12 +2,16 @@
 
 import { precacheAndRoute } from 'workbox-precaching';
 
+// Use the Workbox library to cache assets
 precacheAndRoute(self.__WB_MANIFEST);
 
 self.addEventListener('install', event => {
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open('Tecriut')
-      .then(cache => cache.addAll([
+    caches.open('Tecriut').then(cache => 
+      cache.addAll([
         '/',
         '/index.html',
         '/static/css/main.chunk.css',
@@ -17,15 +21,43 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  // Take control of all available clients
+  event.waitUntil(clients.claim());
+
+  // Clean up old cache versions
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== 'Tecriut') // Only remove caches not matching current cache
+          .map(cacheName => caches.delete(cacheName)) // Delete them
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Cache hit - return the response
-        }
-        // Handle the case when the request is not found in the cache
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      if (response) {
+        // If found in cache, return the response
+        return response;
+      }
+      // Otherwise, fetch from the network
+      return fetch(event.request);
+    })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+// Communicate with the client-side (optional)
+// This can be useful for refreshing the user's page as soon as a new service worker takes control
+self.addEventListener('controllerchange', () => {
+  window.location.reload();
 });
